659a20d6404a9fa1a620a7198e996f127f3b0c10
Nadirah Jama <Nadirahjama@gmail.com>
2022-08-30 22:09:14 -0500

    recreating assignment using react

---
+++ trustone/src/App.js
@@ -0,0 +1,12 @@
+import React, {component} from 'react';
+import { getCountyApi } from '../fetchApi';
+
+function App() {
+  return (
+    <div className="App">
+      {/* fetch data from external api */}  
+      </div>
+  );
+}
+
+export default App;

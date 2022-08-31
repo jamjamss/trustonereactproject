db9824a7ebaa8c74177d52a7b2269debbe9c9993
Nadirah Jama <Nadirahjama@gmail.com>
2022-08-31 11:05:10 -0500

    able to fetch  one county api call

--- trustone/src/App.js
+++ trustone/src/App.js
@@ -7,18 +7,26 @@ import { getCountyApi } from './fetchApi';
 const App = () => {
   
   const [fetchedData, setFetchedData] = React.useState([])
+  const [flattenedFeatures, setFeatures] = React.useState([]);
+  
 
   React.useEffect(() => {
-    getCountyApi().then(({features}) => setFetchedData(features ));
-  }, []);
+    getCountyApi()
+    .then(({features}) => {
+      setFetchedData(features);
+      setFeatures(features.map((features) => features.attributes));
+    })
+    .catch((error) => console.log(error.message));
+ 
+  }, []); 
 
 
   return (
     <div className="App">
-      <pre>{JSON.stringify(fetchedData, null, 2)}</pre>
+      <pre>{JSON.stringify(flattenedFeatures, null, 2)}</pre>
 
       </div>
   );
-}
+} 
 
 export default App;
